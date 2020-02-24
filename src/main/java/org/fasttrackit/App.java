8f2b2e24c2b580242fd23ecf3f77672e4e580278
Daniela Cozmescu <dani_21bcn@hotmail.com>
2020-02-24 20:48:06 +0200

    studied classes, variables, primitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,38 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
+//  Car Race
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the racing game!" );
+
+        Car carReference = new Car();
+        carReference.name = "Audi";
+        carReference.color = "red";
+        carReference.maxSpeed = 260;
+        carReference.mileage = 6.2;
+
+        //concatenation
+        System.out.println("Properties of " + carReference.name);
+        //color red
+        System.out.println("Color: " + carReference.color);
+        // max speed 260
+        System.out.println("Maximum speed: " + carReference.maxSpeed);
+        // mileage 6.2
+        System.out.println("Mileage: " + carReference.mileage);
+        System.out.println("Fuel level: " + carReference.fuelLevel);
+        System.out.println("Racing nr: " + carReference.racingNumber);
+        System.out.println("Damaged: " + carReference.damaged);
+
+        carReference = null;
+
+        Car car2 = new Car();
+        car2.name = "Lamborghini";
+        car2.damaged = true;
+        System.out.println("Properties of " + car2.name);
+        System.out.println("Damaged: " + car2.damaged);
+        System.out.println("Color: " + car2.color);
     }
 }
