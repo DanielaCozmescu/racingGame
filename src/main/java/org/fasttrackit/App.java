2c4a8c122b04e66ec13e536afd525c157b6c13a3
Daniela Cozmescu <dani_21bcn@hotmail.com>
2020-02-24 21:19:04 +0200

    studied classes, variables, primitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -14,6 +14,25 @@ public class App
         carReference.maxSpeed = 260;
         carReference.mileage = 6.2;
 
+//        carReference.engine = new Engine();
+//        carReference.engine.manufacturer = "VW";
+//        carReference.engine.capacity = 2000;
+
+        Engine engine1= new Engine();
+        engine1.manufacturer = "VW";
+        engine1.capacity = 2000;
+
+        carReference.engine=engine1;
+
+        // One object with 2 refrences
+        System.out.println("Engine details: ");
+        System.out.println(engine1.capacity);
+        System.out.println(carReference.engine.capacity);
+        engine1.capacity = 2100;
+        System.out.println("Engine details upd: ");
+        System.out.println(engine1.capacity);
+        System.out.println(carReference.engine.capacity);
+
         //concatenation
         System.out.println("Properties of " + carReference.name);
         //color red
@@ -34,5 +53,6 @@ public class App
         System.out.println("Properties of " + car2.name);
         System.out.println("Damaged: " + car2.damaged);
         System.out.println("Color: " + car2.color);
+
     }
 }
