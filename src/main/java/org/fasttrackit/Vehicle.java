1c71b939ae6873c3e7d01888b0fee447e7953172
Daniela Cozmescu <dani_21bcn@hotmail.com>
2020-03-09 21:25:49 +0200

    studied conditional statements

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -1,5 +1,7 @@
 package org.fasttrackit;
 
+import java.util.SortedMap;
+
 public class Vehicle {
     //instance** variables
     int racingNumber;
@@ -13,6 +15,20 @@ public class Vehicle {
 
     public double accelerate(double speed, double durationInHours){
 
+        if (fuelLevel < = 0){
+            System.out.println("Not enough fuel.");
+            return 0;
+        }
+
+        if (speed > maxSpeed){
+            System.out.println("Max speed exceeded!");
+            return 0;
+        } else if (speed == maxSpeed){
+            System.out.println("Be careful! Max speed reached!");
+        } else {
+            System.out.println("Valid speed entered.");
+        }
+// todo: use more fuel if speed > 120
         System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + " h");
 
         // local variables
