d1c07250883b65dbba1212380fc3341d53e6c564
Daniela Cozmescu <dani_21bcn@hotmail.com>
2020-03-09 20:26:57 +0200

    studied methods

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -8,5 +8,30 @@ public class Vehicle {
     double mileage;
     String color;
     double fuelLevel;
+    double totalDistance;
     boolean damaged;
+
+    public double accelerate(double speed, double durationInHours){
+
+        System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + " h");
+
+        // local variables
+        double travelDistance = speed * durationInHours;
+
+        System.out.println("Travel distance: " + travelDistance);
+
+        totalDistance += travelDistance;
+
+        System.out.println("Total travel distance: " + totalDistance);
+
+        double usedFuel = travelDistance * mileage / 100;
+
+        // fuelLevel = fuelLevel - usedFuel;
+        fuelLevel -=usedFuel;
+
+        System.out.println("Remaining fuel: " + fuelLevel);
+
+        return travelDistance;
+
+    }
 }
